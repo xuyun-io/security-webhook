8ec1f06c5d88ac8ffbc1cee192707fda1d85be8d
pan.xinyuan <1851616111@qq.com>
2023-12-14 10:29:15 +0800

    临时测试使用

--- api/api.go
+++ api/api.go
@@ -1,9 +1,11 @@
 package api
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
-	"security-webhook/api/validating"
+	"io"
+	"net/http"
 	"security-webhook/utils/log"
 )
 
@@ -11,10 +13,39 @@ var certFile = "/etc/certs/tls.crt"
 var keyFile = "/etc/certs/tls.key"
 
 func InitAPI(e *gin.Engine) {
-	e.POST("/validate-privileged-container", validating.PrivilegedContainerCheck)
+	//e.POST("/validate-privileged-container", validating.PrivilegedContainerCheck)
+	//
+	//err := e.RunTLS(":9443", certFile, keyFile)
+	////err := e.Run(":9443")
+	//if err != nil {
+	//	log.Logger.Error("failed to start server", zap.Error(err))
+	//}
 
-	err := e.RunTLS(":9443", certFile, keyFile)
-	//err := e.Run(":9443")
+	e.GET("/proxy", gin.HandlerFunc(func(context *gin.Context) {
+		err := context.Request.ParseForm()
+		if err != nil {
+			log.Logger.Error("ParseForm" + err.Error())
+			return
+		}
+
+		target := context.Request.FormValue("https")
+		fmt.Printf("---->> https://%s", target)
+		tt := fmt.Sprintf("https://%s", target)
+		rsp, err := http.DefaultClient.Get(tt)
+		if err != nil {
+			log.Logger.Error("Get" + err.Error())
+			return
+		}
+
+		data, err := io.ReadAll(rsp.Body)
+		if err != nil {
+			log.Logger.Error("ReadAll" + err.Error())
+			return
+		}
+		context.String(200, "%s", string(data))
+	}))
+
+	err := e.Run(":80")
 	if err != nil {
 		log.Logger.Error("failed to start server", zap.Error(err))
 	}
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module security-webhook
 
-go 1.21
+go 1.20
 
 require (
 	github.com/gin-gonic/gin v1.9.1
